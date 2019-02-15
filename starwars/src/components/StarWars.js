00acbfdeba0bef77bcada0177f09e8459fb9da92
Gill Abada <gill@callifi.com>
2019-02-15 12:35:49 -0500

    add styling.

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
+import './StarWars.css';
+
 const StarWars = props => {
     return (
         <div>
             {props.starwarsChars.map(char => (
                 <ul className="list" key={char.name}>
-                    {char.name}
+                    <h2>{char.name}</h2>
                     <li className="list-item">
                         Birth Year: {char.birth_year}
-                    </li>
+                    </li> <br />
                     <li className="list-item">
                         Eye Color: {char.eye_color}
-                    </li>
+                    </li><br />
                     <li className="list-item">
                         Gender: {char.gender}
-                    </li>
+                    </li><br />
                     <li className="list-item">
                         Hair Color: {char.hair_color}
-                    </li>
+                    </li><br />
                     <li className="list-item">
                         Height: {char.height}
-                    </li>
+                    </li><br />
                     <li className="list-item">
                         Mass: {char.mass}
-                    </li>
+                    </li><br />
                     <li className="list-item">
                         Skin Color: {char.skin_color}
-                    </li>
+                    </li><br />
+                    <img src="https://www.ibm.com/thought-leadership/scienceandstarwars/images/share-lightsaber-blue.jpg" alt="lightsaber"/>
                 </ul>
             ))}
         </div>
