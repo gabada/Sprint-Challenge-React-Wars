8bc56c0c5eb612d1b7774edca4d79b54b716374b
Gill Abada <gill@callifi.com>
2019-02-15 12:04:05 -0500

    import component.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/StarWars';
+import StarWars from './components/StarWars';
 
 class App extends Component {
   constructor() {
