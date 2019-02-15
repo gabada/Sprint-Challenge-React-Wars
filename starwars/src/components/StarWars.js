94c94696dcc3d9b16d0ec66fb69b0308d1565a04
Gill Abada <gill@callifi.com>
2019-02-15 12:05:23 -0500

    update component with list items. and classnames for CSS.

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 const StarWars = props => {
     return (
-        <li>
-            {props.character}
-        </li>
+        <div>
+            {props.starwarsChars.map(char => (
+                <ul className="list" key={char.name}>
+                    {char.name}
+                    <li className="list-item">
+                        Birth Year: {char.birth_year}
+                    </li>
+                    <li className="list-item">
+                        Eye Color: {char.eye_color}
+                    </li>
+                    <li className="list-item">
+                        Gender: {char.gender}
+                    </li>
+                    <li className="list-item">
+                        Hair Color: {char.hair_color}
+                    </li>
+                    <li className="list-item">
+                        Height: {char.height}
+                    </li>
+                    <li className="list-item">
+                        Mass: {char.mass}
+                    </li>
+                    <li className="list-item">
+                        Skin Color: {char.skin_color}
+                    </li>
+                </ul>
+            ))}
+        </div>
     );
 };
 
